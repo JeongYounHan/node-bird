2f8f7b57fb945aa1ebb20852f5f8e4b613d0efb0
JeongYounHan <51sarang@hanmail.net>
2022-04-26 23:14:17 +0900

    redux 사용

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,9 +1,65 @@
 const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "hahah",
+      },
+      content: "첫 번째 게시글 #해시태그 #익스프레스",
+      Images: [
+        {
+          src: "",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: "nero",
+          },
+          content: "얼른 사고싶어요~",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
 };
+
+const dummyPost = {
+  id: 2,
+  User: {
+    id: 2,
+    nickname: "더미더미~",
+  },
+  content: "더미데이터 입니다!",
+  Images: [
+    {
+      src: "",
+    },
+  ],
+  Comments: [
+    {
+      User: {
+        nickname: "nero22",
+      },
+      content: "얼른 사고싶지 않아요~",
+    },
+  ],
+},
+
+const ADD_POST = 'ADD_POST';
+export const addPost = {
+  type: ADD_POST
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+      }
     default:
       return state;
   }
