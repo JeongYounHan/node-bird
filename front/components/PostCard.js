32606e3d683a4346810572db9807861a4d2be9b0
JeongYounHan <51sarang@hanmail.net>
2022-05-22 20:12:36 +0900

    게시글 구현하기

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,5 +1,62 @@
-const PostCard = () => {
-  return <div>PostCard</div>;
+import { Card, Popover, Button, Avatar } from "antd";
+import React, { useState, useCallback } from "react";
+import PropTypes from "prop-types";
+import { RetweetOutlined, HeartTwoTone, HeartOutlined, MessageOutlined, EllipsisOutlined } from "@ant-design/icons";
+import { useSelector } from "react-redux";
+import PostImages from "./PostImages";
+
+const PostCard = ({ post }) => {
+  const { me } = useSelector((state) => state.user);
+  const id = me?.id;
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          liked ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} /> : <HeartOutlined key="heart" onClick={onToggleLike} />,
+          <MessageOutlined key="comment" onClick={onToggleComment} />,
+          <Popover key="more">
+            <Button.Group>
+              {id && post.User.id === id ? (
+                <>
+                  <Button>수정</Button>
+                  <Button type="danger">삭제</Button>
+                </>
+              ) : (
+                <Button>신고</Button>
+              )}
+            </Button.Group>
+            <EllipsisOutlined key="retweet" />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta avatar={<Avatar>{post.User.nickname[0]}</Avatar>} title={post.User.nickname} description={post.content} />
+      </Card>
+      {commentFormOpened && <div>댓글 부분</div>}
+      {/* <CommentForm />
+      <Comments /> */}
+    </div>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.object,
+    Comments: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
 };
 
 export default PostCard;
--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -1,17 +1,21 @@
 import { Button, Form, Input } from "antd";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useRef } from "react";
 import { useSelector } from "react-redux";
 
 const PostForm = () => {
   const { imagePaths } = useSelector((state) => state.post);
+  const imageInput = useRef();
   const [text, onChangeText] = useState("");
   const onSubmit = useCallback(() => {}, []);
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
   return (
     <Form style={{ margin: "10px 0 20px" }} encType="multipart/form-data" onFinish={onSubmit}>
       <Input.TextArea value={text} onChange={onChangeText} maxLength={140} placeholder="어떤 신기한" />
       <div>
-        <input type="file" multiple hidden />
-        <Button>이미지 업로드</Button>
+        <input type="file" multiple hidden ref={imageInput} />
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
         <Button type="primary" style={{ float: "right" }} htmlType="submit">
           짹짹
         </Button>
